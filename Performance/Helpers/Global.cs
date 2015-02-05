3c4d6ceeb622a55603bced0826dfed3f104d639e
titian1987 <titian1987@gmail.com>
2015-02-05 16:17:50 +0100

    Comments

--- Performance/Helpers/Global.cs
+++ Performance/Helpers/Global.cs
@@ -8,11 +8,7 @@ namespace Performance.Helpers
 {
     public class Global
     {
-
-        // Database strings
-        //public const string DBConnectionString = @"Data Source=(LocalDB)\v11.0;" +
-        //                                         @"AttachDbFilename ='C:\Users\yli\Documents\Visual Studio 2013\Projects\Performance\Performance\App_Data\Performance.mdf';" +
-        //                                         @"Integrated Security=True";
+        // db strings
         public static string DBConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
         public const string DBSelectWebUser = @"SELECT [Username] FROM [dbo].[WebUser] WHERE [Username] = @username AND [Password] = @password";

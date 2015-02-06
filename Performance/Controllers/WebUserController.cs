830211ce62f0dca71834b381798f6a887100d392
titian1987 <titian1987@gmail.com>
2015-02-06 12:18:18 +0100

    add missing files which are not in version control

---
+++ Performance/Controllers/WebUserController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Performance.Data.Entity;
+using Performance.DAL;
+
+namespace Performance.Controllers
+{
+    public class WebUserController : Controller
+    {
+        private PerformanceContext db = new PerformanceContext();
+
+        //
+        // GET: /WebUser/
+
+        public ActionResult Index()
+        {
+            return View(db.WebUsers.ToList());
+        }
+
+        //
+        // GET: /WebUser/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            WebUser webuser = db.WebUsers.Find(id);
+            if (webuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(webuser);
+        }
+
+        //
+        // GET: /WebUser/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /WebUser/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(WebUser webuser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WebUsers.Add(webuser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(webuser);
+        }
+
+        //
+        // GET: /WebUser/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            WebUser webuser = db.WebUsers.Find(id);
+            if (webuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(webuser);
+        }
+
+        //
+        // POST: /WebUser/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(WebUser webuser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(webuser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(webuser);
+        }
+
+        //
+        // GET: /WebUser/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            WebUser webuser = db.WebUsers.Find(id);
+            if (webuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(webuser);
+        }
+
+        //
+        // POST: /WebUser/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WebUser webuser = db.WebUsers.Find(id);
+            db.WebUsers.Remove(webuser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Performance/DAL/PerformanceContext.cs
@@ -0,0 +1,24 @@
+﻿using Performance.Data.Entity;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Performance.DAL
+{
+    public class PerformanceContext: DbContext
+    {
+        public PerformanceContext()
+            : base("DefaultConnection") 
+        {
+            //Database.SetInitializer<InitEntity>(new CreateDatabaseIfNotExists<InitEntity>());
+        }
+
+        public DbSet<WebUser> WebUsers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WebUser>().ToTable("WebUser");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

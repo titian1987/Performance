6bc20f57857610ef36504293ec83af637fe7143a
vincentzhengdk <vincent2001@gmail.com>
2015-05-23 17:27:50 +0200

    test

--- Performance/DAL/PerformanceContext.cs
+++ Performance/DAL/PerformanceContext.cs
@@ -12,19 +12,21 @@ namespace Performance.DAL
             //Database.SetInitializer<InitEntity>(new CreateDatabaseIfNotExists<InitEntity>());
         }
 
+
+
         public DbSet<WebUser> WebUsers { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<WebUser>().ToTable("WebUser");
-            modelBuilder.Entity<HoldingTable>().ToTable("HoldingTable");
-            modelBuilder.Entity<InvestmentTree>().ToTable("InvestmentTree");
-
-            base.OnModelCreating(modelBuilder);
-        }
-
         public DbSet<HoldingTable> HoldingTables { get; set; }
-
         public DbSet<InvestmentTree> InvestmentTrees { get; set; }
+        public DbSet<Measurement> Mearesurements { get; set; }
+        public DbSet<Test> Tests { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    //modelBuilder.Entity<WebUser>().ToTable("WebUser");
+        //    //modelBuilder.Entity<HoldingTable>().ToTable("HoldingTable");
+        //    //modelBuilder.Entity<InvestmentTree>().ToTable("InvestmentTree");
+        //    //modelBuilder.Entity<Measurement>()
+        //    base.OnModelCreating(modelBuilder);
+        //}
     }
 }
---
+++ Performance/Migrations/201505161450538_AddTest.cs
@@ -0,0 +1,26 @@
+namespace Performance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTest : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tests",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TestName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tests");
+        }
+    }
+}
